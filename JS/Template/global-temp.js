a25faeb1c671d1f7a687499e90059a4a30e2e0dc
Firefenyxx <michmojunk@gmail.com>
2025-10-17 14:19:32 +0200

    nav template

---
+++ JS/Template/global-temp.js
@@ -0,0 +1,14 @@
+import { MENU } from "./nav.js";
+import {FOOT} from "./foot.js" ;
+
+
+// document.body.innerHTML = MENU + FOOT;
+
+// const display = 
+// document.createElement(MENU);
+document.body.appendChild(MENU);
+
+console.log(MENU);
+// console.log(FOOT);
+// console.log("hey")
+console.log("holaa");
