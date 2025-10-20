1730b77b1fbc5a8e06dd8f281d2c6283f77f4bb0
Firefenyxx <michmojunk@gmail.com>
2025-10-20 14:20:10 +0200

    Synopsis structure start

--- JS/Template/global-temp.js
+++ JS/Template/global-temp.js
@@ -1,20 +1,42 @@
 import { MENU } from "./nav.js";
 import {FOOT} from "./foot.js" ;
 
+//Metemos el nav y el footer
+// const NAV = document.createElement(`span`);
+// NAV.innerHTML= `${MENU}`;
+// document.getElementById("nav").appendChild(NAV);
 
-// document.body.innerHTML = MENU + FOOT;
+// console.log(NAV);
 
-// const display = 
-// document.createElement(MENU);
-const NAV = document.createElement(`span`);
-NAV.innerHTML= `${MENU}`;
-document.getElementById("nav").appendChild(NAV);
-
-console.log(NAV);
-
-const FOOTE = document.createElement(`footer`);
-FOOTE.innerHTML= `${FOOT}`;
-document.getElementById("foot").appendChild(FOOTE);
+// const FOOTE = document.createElement(`footer`);
+// FOOTE.innerHTML= `${FOOT}`;
+// document.getElementById("foot").appendChild(FOOTE);
 // console.log(FOOT);
-// console.log("hey")
-console.log("holaa");
+
+// Boton de Know more
+    // let basicText = document.getElementsByClassName("cheat");
+    // let moreText = document.getElementsByClassName("know_more");
+    // let botonText = document.getElementsByClassName("know");
+ let basicText = document.getElementsByClassName("cheat")
+    let moreText = document.getElementsByClassName("know_more");
+    let botonText = document.getElementsByClassName("know");
+
+
+function know() {
+   
+if 
+(basicText.style.display ==="none"){
+    basicText.style.display ="inline"
+    botonText.innerHTML="Know More";
+    moreText.style.display = "none";
+}
+else {
+    botonText.innerHTML="Show Less";
+    moreText.style.display = "inline";
+    basicText.style.display ="none"
+
+}
+}
+console.log(basicText)
+console.log(botonText)
+console.log(moreText)
