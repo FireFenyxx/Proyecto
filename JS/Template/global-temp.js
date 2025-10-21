7a1bac4e54777a0220e6c1e64ad8de22588a7196
Firefenyxx <michmojunk@gmail.com>
2025-10-21 10:55:58 +0200

    Know buttons fixed

--- JS/Template/global-temp.js
+++ JS/Template/global-temp.js
@@ -1,42 +1,50 @@
 import { MENU } from "./nav.js";
-import {FOOT} from "./foot.js" ;
+import { FOOT } from "./foot.js";
 
-//Metemos el nav y el footer
-// const NAV = document.createElement(`span`);
-// NAV.innerHTML= `${MENU}`;
-// document.getElementById("nav").appendChild(NAV);
-
-// console.log(NAV);
-
-// const FOOTE = document.createElement(`footer`);
-// FOOTE.innerHTML= `${FOOT}`;
-// document.getElementById("foot").appendChild(FOOTE);
-// console.log(FOOT);
-
-// Boton de Know more
-    // let basicText = document.getElementsByClassName("cheat");
-    // let moreText = document.getElementsByClassName("know_more");
-    // let botonText = document.getElementsByClassName("know");
- let basicText = document.getElementsByClassName("cheat")
-    let moreText = document.getElementsByClassName("know_more");
-    let botonText = document.getElementsByClassName("know");
+// Metemos el nav y el footer --------------------------------
 
 
-function know() {
-   
-if 
-(basicText.style.display ==="none"){
-    basicText.style.display ="inline"
-    botonText.innerHTML="Know More";
-    moreText.style.display = "none";
-}
-else {
-    botonText.innerHTML="Show Less";
-    moreText.style.display = "inline";
-    basicText.style.display ="none"
+const NAV = document.createElement(`span`);
+NAV.innerHTML = `${MENU}`;
+document.getElementById("nav").appendChild(NAV);
 
-}
-}
-console.log(basicText)
-console.log(botonText)
-console.log(moreText)
+console.log(NAV);
+
+const FOOTE = document.createElement(`footer`);
+FOOTE.innerHTML = `${FOOT}`;
+document.getElementById("foot").appendChild(FOOTE);
+console.log(FOOT);
+
+// Boton de Know more --------------------
+
+//     let moreText = document.getElementsByClassName("know_more");
+//     let botonText = document.getElementsByClassName("know");
+
+
+// let moreText = document.querySelectorAll("know_more");
+// let botonText = document.querySelectorAll("know");
+
+const lessContent = `<img src="Imagenes/Minus.png" alt="Minus"> Show Less`
+const moreContent = `<img src="Imagenes/Plus.png" alt="plus"> Know More`
+
+    document.querySelectorAll(".know").forEach(boton => {
+        boton.addEventListener(`click`, () => {
+            const extraClick = boton.previousElementSibling
+            if (extraClick.style.display === 'none' || extraClick.style.display === '') {
+                extraClick.style.display = 'inline';
+                boton.innerHTML = `${lessContent}`;
+            } else {
+                extraClick.style.display = 'none';
+                boton.innerHTML = `${moreContent}`;
+            }
+        });
+    });
+
+
+
+
+
+
+
+// console.log(botonText)
+// console.log(moreText)
