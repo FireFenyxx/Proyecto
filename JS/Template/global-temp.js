654a1827b509070fb21e3e3f3eb2c2a2409d8ab4
Firefenyxx <michmojunk@gmail.com>
2025-10-22 10:43:34 +0200

    Removal of arrow functions

--- JS/Template/global-temp.js
+++ JS/Template/global-temp.js
@@ -17,34 +17,19 @@ console.log(FOOT);
 
 // Boton de Know more --------------------
 
-//     let moreText = document.getElementsByClassName("know_more");
-//     let botonText = document.getElementsByClassName("know");
-
-
-// let moreText = document.querySelectorAll("know_more");
-// let botonText = document.querySelectorAll("know");
-
 const lessContent = `<img src="Imagenes/Minus.png" alt="Minus"> Show Less`
 const moreContent = `<img src="Imagenes/Plus.png" alt="plus"> Know More`
 
-    document.querySelectorAll(".know").forEach(boton => {
-        boton.addEventListener(`click`, () => {
-            const extraClick = boton.previousElementSibling
-            if (extraClick.style.display === 'none' || extraClick.style.display === '') {
-                extraClick.style.display = 'inline-flex';
-                boton.innerHTML = `${lessContent}`;
-            } else {
-                extraClick.style.display = 'none';
-                boton.innerHTML = `${moreContent}`;
-            }
-        });
+document.querySelectorAll(".know").forEach(function(boton) {
+    boton.addEventListener('click', function() {
+        var extraClick = boton.previousElementSibling;
+        if (extraClick.style.display === 'none' || extraClick.style.display === '') {
+            extraClick.style.display = 'inline-flex';
+            boton.innerHTML = `${moreContent}`
+        } else {
+            extraClick.style.display = 'none';
+            boton.innerHTML =`${lessContent}`; 
+        }
     });
+});
 
-
-
-
-
-
-
-// console.log(botonText)
-// console.log(moreText)
